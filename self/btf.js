dc2730018c6894a3c1421b4922833f545455e41a
even629 <zhaohang731005515@proton.me>
2025-04-18 16:09:28 +0800

    Site updated: 2025-04-18 16:09:23

--- self/btf.js
+++ self/btf.js
@@ -1,1 +1,31 @@
-(()=>{const e=e=>e.includes("/en/");window.loadFullPage=e=>{window.location.href=e};const o=e(window.location.href),l=o?document.querySelectorAll('a[href^="https://even629.com/"]'):document.querySelectorAll('a[href^="/en/"]');var r;r=o,l.forEach((o=>{r&&e(o.href)||(o.href=`javascript:loadFullPage('${o.href}');`)}))})();
+(() => {
+  // 判断是否为英文
+  const isIncludeEN = item => {
+    const key = '/en/'
+    return item.includes(key)
+  }
+
+  // 建立 重新导向到不同语言的 url 
+  window.loadFullPage = (url) => {
+    window.location.href = url
+  }
+
+  // 重新导向
+  const eventFn = (elements, includeEN) => {
+    elements.forEach(item => {
+      if (!includeEN || !isIncludeEN(item.href)) {
+        item.href = `javascript:loadFullPage('${item.href}');`
+      }
+    })
+  }
+
+  // 判断目前是否为英文
+  const nowIncludeEN = isIncludeEN(window.location.href)
+
+  // 修改你的 url
+  const selector = nowIncludeEN
+    ? document.querySelectorAll('a[href^="https://even629.com/"]') // 只需要修改这一行就好了，将https://<your-url>替换为你的blog网址。
+    : document.querySelectorAll('a[href^="/en/"]')
+
+  eventFn(selector, nowIncludeEN)
+})()
